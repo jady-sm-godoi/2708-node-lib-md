6caf82921e9107c53458bfa71c37160efdf7b501
jady-sm-godoi <jady.s.m@gmail.com>
2023-06-05 21:21:26 -0300

    processando diretórios

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,28 @@
-import catchFile from "./index.js";
 import chalk from "chalk";
+import fs from 'fs';
+import catchFile from "./index.js";
 
 const way = process.argv;
 
-async function textProcessor(someWay){
-    const file = await catchFile(someWay[2])
-    console.log(chalk.yellow('lista de links: '), file)
+function showResult(list){
+    console.log(chalk.yellow('lista de links: '), list)
+}
+
+async function textProcessor(args){
+    const someWay = args[2]
+
+    if(fs.lstatSync(someWay).isFile()){
+        const file = await catchFile(someWay)
+        showResult(file)
+    } else if(fs.lstatSync(someWay).isDirectory()){
+        const files = await fs.promises.readdir(someWay)
+        files.forEach(async(f) => {
+            const result = await catchFile(`${someWay}/${f}`)
+            showResult(result)
+        });
+    }
+    
+
 }
 
 textProcessor(way)
